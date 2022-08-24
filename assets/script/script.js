2131fe3db817a67b68dceb939943d795140eafd6
lxpionage <anthonyhod11@gmail.com>
2022-08-23 21:33:15 -0400

    added questions

---
+++ assets/script/script.js
@@ -0,0 +1,21 @@
+var questions = [{
+    question: "1. What do you most pay attention to while watching a movie?",
+    choices: ["Performances", "Action", "Set Design", "Jokes"],
+    
+}, {
+    question: "2. Which of these titles piques your interest?",
+    choices: ["Pretty Woman", "Ex Machina", "The Wailing", "Guns Akimbo"],
+    
+}, {
+    question: "3. Out of these choices, which actor do you like the most?",
+    choices: ["Sigourney Weaver", "Jim Carrey", "Arnold Schwarzenegger", "Harrison Ford"],
+   
+}, {
+    question: "4. What is your favorite movie franchise?",
+    choices: ["Star Wars", "Saw", "Rambo", "American Pie"],
+    
+}, {
+    question: "5. Which movie character would you most like to get lunch with",
+    choices: ["Hannibal Lector", "The Terminator", "Clark Griswold","Agent Smith"],
+   
+}];
